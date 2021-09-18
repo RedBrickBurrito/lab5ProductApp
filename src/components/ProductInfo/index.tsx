7118577b109c2d8ff8f2fe77e87abf09bea7550d
Eddy <a01568010@gmail.com>
2021-09-18 15:56:44 -0600

    A

--- src/components/ProductInfo/index.tsx
+++ src/components/ProductInfo/index.tsx
@@ -1,5 +1,5 @@
 import "./index.css";
-import { Grid, Paper, Typography } from "@material-ui/core";
+import { Grid, Paper, Typography, Select, MenuItem, InputLabel, Button } from "@material-ui/core";
 const ProductInfo = () => {
     return (
         <div className="productInfo">
@@ -30,6 +30,53 @@ const ProductInfo = () => {
                         <Typography className="dollars">
                             706.93
                         </Typography>
+                    </Grid>
+                    <Grid item lg={8} />
+                    <Grid item lg={2}>
+                        <InputLabel id="color-label" className="productLabel">Select color</InputLabel>
+                        <Select
+                            labelId="color-label"
+                            id="color-select"
+                            label="color"
+                        >
+                            <MenuItem value="BLK">Black</MenuItem>
+                            <MenuItem value="BLU">Blue</MenuItem>
+                            <MenuItem value="BWN">Brown</MenuItem>
+                        </Select>
+                    </Grid>
+                    <Grid item lg={2}>
+                        <InputLabel id="size-label" className="productLabel">Select size</InputLabel>
+                        <Select
+                            labelId="size-label"
+                            id="size-select"
+                            label="Size"
+                        >
+                            <MenuItem value="30x32">30x32</MenuItem>
+                            <MenuItem value="33x30">33x30</MenuItem>
+                            <MenuItem value="32x32">32x32</MenuItem>
+                            <MenuItem value="34x32">34x32</MenuItem>
+                            <MenuItem value="36x32">36x32</MenuItem>
+                        </Select>
+                    </Grid>
+                    <Grid item lg={8} />
+                    <Grid item lg={2}>
+                        <InputLabel id="quantity-label" className="productLabel">Select quantity</InputLabel>
+                        <Select
+                            labelId="quantity-label"
+                            id="quantity-select"
+                            label="quantity"
+                        >
+                            <MenuItem value="1">1</MenuItem>
+                            <MenuItem value="2">2</MenuItem>
+                            <MenuItem value="3">3</MenuItem>
+                            <MenuItem value="4">4</MenuItem>
+                            <MenuItem value="5">5</MenuItem>
+                        </Select>
+                    </Grid>
+                    <Grid item lg={10} />
+
+                    <Grid item lg={4}>
+                        <Button variant="contained" className="addCartButton">Add to cart</Button>
                     </Grid>
                     <Grid item lg={12} />
                 </Grid>
