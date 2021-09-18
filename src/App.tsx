d9ec6d6f8524d7ead4a4fb1cf828e567aeaeb625
Eddy <a01568010@gmail.com>
2021-09-18 17:46:39 -0600

    B

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import ProductDetail from './containers/ProductDetail';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import Header from "./containers/Header";
 
 function App() {
   return (
     <React.Fragment>
       <CssBaseline />
+      <Header />
       <ProductDetail />
     </React.Fragment>
   );
